de7b532ab828786e1f286c70dcf6eaa3f738bfab
D3r3kR <D3r3kR@users.noreply.github.com>
2020-05-03 03:01:52 -0500

    bot and background
    bot does nothing just spawns and added a background for future use.

---
+++ src/Platformer/Bot.java
@@ -0,0 +1,106 @@
+package Platformer;
+
+
+public class Bot extends Enemy {
+    private int maxHealth, maxMana, healthPoints, manaPoints, weaponIndex, mvTimer;
+
+    public Bot(int x, int y, int w, int h, int i) {
+        super(x, y, w, h, i);
+        this.weaponIndex = i;
+        maxHealth = 0;
+        maxMana = 0;
+        healthPoints = 0;
+        manaPoints = 0;
+        setStats(10, 100, 20, 33);//Health Points, Mana Points, Walk Speed, Jump Height
+    }
+
+    void remove() {
+
+    }
+
+    void spawn() {
+
+    }
+
+    public String[] getImgSources() {
+        //List of all assassin image sources. Index in this list correlates to imgIndex.
+        return new String[]{"src/Platformer/Images/Assassin/assassinNeutral.PNG",
+                "src/Platformer/Images/Assassin/assassinUp.PNG", "src/Platformer/Images/Assassin/assassinDown.png",
+                "src/Platformer/Images/Assassin/assassinRunning1.PNG", "src/Platformer/Images/Assassin/assassinRunning2.PNG",
+                "src/Platformer/Images/Assassin/assassinRunning3.PNG", "src/Platformer/Images/Assassin/assassinRunning4.PNG",
+                "src/Platformer/Images/Assassin/assassinRunning5.PNG", "src/Platformer/Images/Assassin/assassinRunning6.PNG",
+                "src/Platformer/Images/Assassin/assassinRunning7.PNG", "src/Platformer/Images/Assassin/assassinRunning8.PNG",
+                "src/Platformer/Images/Assassin/assassinSkid.PNG", "src/Platformer/Images/Assassin/assassinSkidBack.PNG",
+                "src/Platformer/Images/Assassin/assassinCrouch.PNG"};
+    }
+
+    public void setImgIndex(int[] nums) {
+        imgIndex = nums;
+    }
+
+    public int getImgIndex() {
+        //Checks if player is in the air
+        if (vSpd >= 0 && !canJump) {
+            return (imgIndex[2]);
+        }
+        if (vSpd < 0) {
+            return (imgIndex[1]);
+        }
+
+        //Checks if player is crouching and resets running animation
+        if (downState) {
+            mvTimer = 0;
+            return imgIndex[13];
+        }
+        //Checks if player staying still
+        if (Math.abs(hSpd) < 1 || mvTimer == 0) {
+            return imgIndex[0];
+        }
+        //Player is on ground moving. Animation of 8 frames running at 12 frames per second
+        else {
+            //Skidding Animation & Skidding backwards animation. Resets running animation
+            if (!leftState && !rightState) {
+                mvTimer=0;
+                if ((hSpd > 0 && facingRight) || (hSpd < 0 && !facingRight))
+                    return imgIndex[11];
+                return imgIndex[12];
+            }
+            //Iterates through the running animation
+            int mvMod = mvTimer % 40;
+            if (mvMod == 0)
+                return imgIndex[10];
+            if (mvMod <= 5)
+                return imgIndex[3];
+            if (mvMod <= 10)
+                return imgIndex[4];
+            if (mvMod <= 15)
+                return imgIndex[5];
+            if (mvMod <= 20)
+                return imgIndex[6];
+            if (mvMod <= 25)
+                return imgIndex[7];
+            if (mvMod <= 30)
+                return imgIndex[8];
+            if (mvMod <= 35)
+                return imgIndex[9];
+            return imgIndex[10];
+        }
+    }
+
+    public void step() {
+        super.step();                //Calls Player.step()
+        if (hSpd != 0 && canJump)    //Checks if player is on the ground running
+            mvTimer++;                //Iterates through running frames
+        else
+            mvTimer = 0;            //Resets animation once player stops
+    }
+
+    public void setStats(int totHealth, int totMana, int theWalkSpeed, int theJumpHeight) {
+        this.maxHealth = totHealth;
+        this.maxMana = totMana;
+        this.healthPoints = totHealth;
+        this.manaPoints = totMana;
+        this.walkSpeed = theWalkSpeed;
+        this.jumpHeight = theJumpHeight;
+    }
+}
---
+++ src/Platformer/Enemy.java
@@ -0,0 +1,284 @@
+package Platformer;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.util.Random;
+
+public abstract class Enemy implements Entity {
+    /* Variables
+     * x & y - Position of Player
+     * w & h - Dimensions of Player (Hit box)
+     * walkSpeed - Horizontal Distance Player Travels each step
+     * weapIndex - Which weapon player uses 0, 1, or 2
+     * grv & hAcc - Gravity (Vertical Acceleration) and horizontal acceleration
+     * vSpd & hSpd - Speed player is travelling (Vertical and Horizontal)
+     * upState, downState, rightState, & leftState - Detects of keys that are being pressed
+     * room - The room the player is in
+     * imgIndex - The indexes the room uses to display images
+     * canJump - True if player can jump. If player is in the air, it is false */
+    protected int x, y, w, h, walkSpeed, weapIndex, healthPoints, manaPoints, maxHealth, maxMana;
+    protected double grv, vSpd, hSpd, jumpHeight, hAcc;
+    protected boolean upState = false, rightState = false, leftState = false, downState = false, facingRight = true,
+            canJump = false;
+    protected Room room;
+    protected int[] imgIndex;
+    Random rand;
+
+    //Sets Variables
+    public Enemy(int x, int y, int w, int h, int i) {
+        walkSpeed = 13;
+        healthPoints = 100;
+        manaPoints = 100;
+        hSpd = 0;
+        vSpd = 0;
+        jumpHeight = 0;
+        maxHealth = 100;
+        maxMana = 100;
+        grv = 1.5;
+        hAcc = 2;
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        Random rand = new Random();
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    abstract void remove();
+
+    abstract void spawn();
+
+    public int getWeapIndex() {
+        return weapIndex;
+    }
+
+    public void setWeapIndex(int weap) {
+        weapIndex = weap;
+    }
+
+    int getHealth() {
+        return this.healthPoints;
+    }
+
+    public boolean facingRight() {
+        return facingRight;
+    }
+
+    void setHealth(int damage) {
+        this.healthPoints -= damage;    //subtracts damage make damage negative for heals
+        if (this.healthPoints > this.maxHealth) {
+            this.healthPoints = this.maxHealth;
+        }
+    }
+
+    int getMana() {
+        return this.manaPoints;
+    }
+
+    void setMana(int cost) {
+        this.manaPoints -= cost;            //subtracts mana used, make negative for regened mana
+        if (this.manaPoints > this.maxMana) {
+            this.manaPoints = this.maxMana;
+        }
+    }
+
+    //Handles Collision and Movement of Player
+    public void step() {
+        int rightInt = 0;    //Used to determine Direction player is moving
+        int leftInt = 0;     //Used to determine Direction player is moving
+        if (rightState)
+            rightInt = 1;
+        if (leftState)
+            leftInt = 1;
+        int move = rightInt - leftInt; //-1 = left, 0 = still, 1 = right
+
+        if (move == -1) {
+            facingRight = false;
+        }
+        if (move == 1) {
+            facingRight = true;
+        }
+
+        //Implements player's acceleration on the ground. Air acceleration is 2 times ground acceleration
+        //Max speed is equal to walk speed
+        if (canJump)
+            hAcc = move * 2;
+        else
+            hAcc = move;
+
+        //Implements faster deceleration when player is crouched
+        if (downState && canJump) {
+            hAcc = 0;
+            hSpd *= 0.85;
+            if (Math.abs(hSpd) < 1)
+                hSpd = 0;
+        }
+
+        //The deceleration when the player is not crouched
+        else if (move == 0) {
+            hSpd = 0.9 * hSpd;
+            if (Math.abs(hSpd) < 1)
+                hSpd = 0;
+        }
+
+        //Increases/Decreases hSpd by hAcc
+        hSpd += hAcc;
+
+        //Limits speed of player
+        if (walkSpeed < Math.abs(hSpd))
+            hSpd = sign(hSpd) * walkSpeed;
+
+        //Changes vSpd for gravity
+        vSpd += grv;
+
+        //Used to determine if player can jump or is in the air
+        canJump = false;
+        //Checks all entities for Platforms
+        for (Entity i : room.entityList) {
+            if (i instanceof Platform) {
+                //Checks if Platform is directly below. If so, player can jump. Stays true once it becomes true
+                if (!canJump) {
+                    canJump = i.getBounds().intersects(new Rectangle(x, y + 1, w, h));
+                }
+                if (canJump && upState) {
+                    vSpd = jumpHeight * -1; //Sends player upward (Jump)
+                    downState = false;
+                }
+                /* Checks if player will collide with a platform in the next step. If so, it
+                 * will move the player as close to the platform as possible without intersecting it.
+                 * Then, it sets vSpd/hSpd to 0, so it will not move in the direction. */
+                if (i.getBounds().intersects(new Rectangle(x + (int) hSpd, y, w, h))) {
+                    while (!i.getBounds().intersects(new Rectangle(x + (int) sign(hSpd), y, w, h))) {
+                        x += sign((int) hSpd);
+                    }
+                    hSpd = 0;
+                }
+                if (i.getBounds().intersects(new Rectangle(x, y + (int) vSpd, w, h))) {
+                    while (!i.getBounds().intersects(new Rectangle(x, y + (int) sign(vSpd), w, h))) {
+                        y += sign((int) vSpd);
+                    }
+                    vSpd = 0;
+                }
+                //Tests if player is inside a platform, and pushes player horizontally out of the shortest side
+                if (i.getBounds().intersects(new Rectangle(x, y, w, h))) {
+                    int distRight = Math.abs((int) (i.getBounds().getX() - (x + w)));
+                    int distLeft = Math.abs((int) (x - (i.getBounds().getX() + i.getBounds().getWidth())));
+                    if (distRight < distLeft) {
+                        x = (int) (i.getBounds().getX() - w);
+                    } else
+                        x = (int) (i.getBounds().getX() + i.getBounds().getWidth());
+                }
+            }
+        }
+
+        if (downState && h == 200 && canJump) {
+            h = 100;
+            y += 100;
+        } else if (!downState && h == 100) {
+            h = 200;
+            y -= 100;
+        }
+        //Changes player's position
+        x += hSpd;
+        y += vSpd;
+
+    }
+
+    //Displays player (Placeholder until we add a real sprite)
+    public void paint(Graphics2D g) {
+        g.setColor(new Color(74, 10, 111, 100));
+        if (canJump)
+            g.setColor(new Color(74, 10, 111));
+        g.fillRect(x - room.getCamX(), y, w, h);
+    }
+
+    //Returns the enemy's' hit box
+    public Rectangle getBounds() {
+        return new Rectangle(x, y, w, h);
+    }
+
+    //Checks if keys are down.
+    public void keyPressed(KeyEvent k) {
+        int move= rand.nextInt(3);
+        if (move == 0) {
+            upState = true;
+            leftState = false;
+            rightState = false;
+            downState = false;
+
+        }
+        if (move == 1) {
+            rightState = true;
+            upState = false;
+            leftState = false;
+            downState = false;
+        }
+        if (move == 2) {
+            upState = false;
+            leftState = false;
+            rightState = false;
+            downState = false;
+        }
+        if (move == 3) {
+            upState = false;
+            leftState = false;
+            rightState = false;
+            downState = false;
+        }
+        /*
+        if (k.getKeyCode() == KeyEvent.VK_UP) {
+            upState = true;
+        }
+        if (k.getKeyCode() == KeyEvent.VK_RIGHT) {
+            rightState = true;
+        }
+        if (k.getKeyCode() == KeyEvent.VK_LEFT) {
+            leftState = true;
+        }
+        if (k.getKeyCode() == KeyEvent.VK_DOWN) {
+            downState = true;
+        }
+         */
+
+    }
+
+    //Returns 1, -1, or 0 depending on the sign of the input
+    private double sign(double num) {
+        if (num != 0)
+            return (num / Math.abs(num));
+        return (0);
+    }
+
+    //Returns X position
+    public int getX() {
+        return x;
+    }
+
+    //Returns Y Position
+    public int getY() {
+        return y;
+    }
+
+    //Returns All Image Sources in Order
+    public String[] getImgSources() {
+        return new String[]{"src/Platformer/Images/stockImage.png"};
+    }
+
+    //Sets the list of indexes to display images
+    public void setImgIndex(int[] nums) {
+        imgIndex = nums;
+    }
+
+    //Contains logic for which image is to be displayed at a point in time
+    public int getImgIndex() {
+        return (imgIndex[0]);
+    }
+
+    //Returns whether or not a player can jump
+    public boolean canJump() {
+        return canJump;
+    }
+}
