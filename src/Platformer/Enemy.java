485630272f169b73c7f2f73d0bf1b0e734519f62
D3r3kR <D3r3kR@users.noreply.github.com>
2020-05-04 23:06:44 -0500

    put enemy step into bot

--- src/Platformer/Enemy.java
+++ src/Platformer/Enemy.java
@@ -12,6 +12,7 @@ public abstract class Enemy implements Entity { //Enemy-- does not have mana
     protected boolean leftState, rightState, upState, canJump;
     public Room room;
     protected int[] imgIndex;
+    protected HealthBars HB;
 
     public Enemy(int x, int y, int w, int h) {
         walkSpeed = 13;
@@ -33,93 +34,15 @@ public abstract class Enemy implements Entity { //Enemy-- does not have mana
         boolean facingRight = false;
     }
 
-    public void setRoom(Room room) {
-        this.room = room;
-    }
-
     abstract void remove();
 
     abstract void spawn();
 
-    void setHealth(int damage) {
-
-    }
-
     public void step() {
-        upState = false;
-        int vrand = (int) (Math.random() * 250);// 0-2
-        int hrand = (int) (Math.random() * 500);//0-999
-
-        //horizontal decisions
-        if (leftState) {
-            if (hrand == 0) {
-                leftState = false;
-                rightState = false;
-            } else if (hrand == 1) {
-                leftState = false;
-                rightState = true;
-            }
-        } else if (rightState) {
-
-            if (hrand == 0) {
-                rightState = false;
-            } else if (hrand == 1) {
-                leftState = true;
-                rightState = false;
-            }
-        } else {
-            if (hrand == 0) {
-                leftState = true;
-                rightState = false;
-            } else if (hrand == 1) {
-                rightState = true;
-            }
+        this.HB.step();
+        if (this.healthPoints <= 0) {
+            this.remove();
         }
-        //vertical decisions
-        if (vrand == 0) {
-            upState = true;
-        }
-        direction(leftState, rightState);
-        vSpd += grv;
-        canJump = false;
-        for (Entity i : room.entityList) {
-            if (i instanceof Platform) {
-                //Checks if Platform is directly below. If so, player can jump. Stays true once it becomes
-                if (!canJump) {
-                    canJump = i.getBounds().intersects(new Rectangle(x, y + 1, w, h));
-                }
-                if (canJump && upState) {
-                    vSpd = jumpHeight * -1; //Sends player upward (Jump)
-                    upState = false;
-
-                }
-                /* Checks if player will collide with a platform in the next step. If so, it
-                 * will move the player as close to the platform as possible without intersecting it.
-                 * Then, it sets vSpd/hSpd to 0, so it will not move in the direction. */
-                if (i.getBounds().intersects(new Rectangle(x + (int) hSpd, y, w, h))) {
-                    while (!i.getBounds().intersects(new Rectangle(x + (int) sign(hSpd), y, w, h))) {
-                        x += sign((int) hSpd);
-                    }
-                    hSpd = 0;
-                }
-                if (i.getBounds().intersects(new Rectangle(x, y + (int) vSpd, w, h))) {
-                    while (!i.getBounds().intersects(new Rectangle(x, y + (int) sign(vSpd), w, h))) {
-                        y += sign((int) vSpd);
-                    }
-                    vSpd = 0;
-                }
-                //Tests if player is inside a platform, and pushes player horizontally out of the shortest side
-                if (i.getBounds().intersects(new Rectangle(x, y, w, h))) {
-                    int distRight = Math.abs((int) (i.getBounds().getX() - (x + w)));
-                    int distLeft = Math.abs((int) (x - (i.getBounds().getX() + i.getBounds().getWidth())));
-                    if (distRight < distLeft) {
-                        x = (int) (i.getBounds().getX() - w);
-                    } else
-                        x = (int) (i.getBounds().getX() + i.getBounds().getWidth());
-                }
-            }
-        }
-
         for (Hurtbox i : room.hurtboxList){
             if(i.parent instanceof Player){
                 if(i.getBounds().intersects(new Rectangle(x, y, w, h))){
@@ -127,27 +50,7 @@ public abstract class Enemy implements Entity { //Enemy-- does not have mana
                 }
             }
         }
-
-        this.x += hSpd;
-        this.y += vSpd;
     }
-
-    private double sign(double num) {
-        if (num != 0)
-            return (num / Math.abs(num));
-        return (0);
-    }
-
-    public double direction(boolean leftState, boolean rightState) {
-        int dir = 0;
-        if (leftState) {
-            dir += -1;
-        } else if (rightState) {
-            dir += 1;
-        }
-        return sideMovement(dir);
-    }
-
     public double sideMovement(int dir) {
         if (Math.abs(hSpd) < walkSpeed) {
             hSpd += hAcc * dir;
@@ -162,6 +65,8 @@ public abstract class Enemy implements Entity { //Enemy-- does not have mana
         g.fillRect(x - room.getCamX(), y, w, h);
     }
 
+    //---Getters and Setters---
+
     //Returns the enemy's hit box
     public Rectangle getBounds() {
         return new Rectangle(x, y, w, h);
@@ -175,6 +80,10 @@ public abstract class Enemy implements Entity { //Enemy-- does not have mana
     //Returns Y Position
     public int getY() {
         return y;
+    }
+    //Sets the room
+    public void setRoom(Room room) {
+        this.room = room;
     }
 
     //Returns All Image Sources in Order
@@ -202,4 +111,5 @@ public abstract class Enemy implements Entity { //Enemy-- does not have mana
     int getTotalHealth(){
         return this.maxHealth;
     }
+    void setHealth(int damage) { this.healthPoints -= damage;}
 }
